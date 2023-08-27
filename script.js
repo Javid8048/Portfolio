478d510cb2719969464b898ff50dc53f5830c061
JAVID ASHWAQ F <112797329+Javid8048@users.noreply.github.com>
2023-08-27 07:47:44 +0000

    commit

--- script.js
+++ script.js
@@ -10,5 +10,25 @@ document.getElementById("darkMode").addEventListener("click", ()=> {
 document.querySelector("#lightMode").addEventListener('click', ()=> {
     document.documentElement.style.setProperty("--background-odd", "#250");
     document.documentElement.style.setProperty("--background-even", "#254");
-})
+});
 
+let sec = document.querySelectorAll('section');
+let links = document.querySelectorAll('header nav a');
+
+window.onload = () => 
+
+window.onscroll = () => {
+    sec.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top>=offset && top<offset + height) {
+            links.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+            })
+        }
+    })
+}
